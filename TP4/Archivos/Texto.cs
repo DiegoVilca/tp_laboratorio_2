1e8193ffacfb5bc2bd2d5c9e0d2b6def82dee2eb
diegovilca <diegovilca@gmail.com>
2016-11-18 03:32:34 -0300

    TP 4

---
+++ TP4/Archivos/Texto.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Archivos
+{
+    public class Texto : IArchivo<string>
+    {
+        private string archivo;
+        public Texto(string archivo)
+        {
+            this.archivo = archivo;
+        }
+
+        public bool guardar(string datos)
+        {
+            bool bandera = true;
+            StreamWriter escritor = null; 
+
+            try
+            {
+                escritor = new StreamWriter(this.archivo, true);  
+                escritor.WriteLine(datos);              
+
+
+                bandera = true;
+            }
+            catch (Exception)
+            {
+
+                bandera = false;
+            }
+            finally
+            {
+                escritor.Close();
+            }
+
+            return bandera;
+        }
+
+        public bool leer(out List<string> datos)
+        {
+            bool bandera;
+            datos = new List<string>();
+
+            try
+            {
+                using (StreamReader lector = new StreamReader(this.archivo))  
+                {
+                    while (!lector.EndOfStream)
+                    {
+                        datos.Add(lector.ReadLine());
+                    }
+
+                    bandera = true;
+                }
+
+            }
+            catch (Exception)
+            {
+                bandera = false;
+            }
+
+            return bandera;
+        }
+    }
+}
---
+++ TP4/Hilo/Descargador.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net; // Avisar del espacio de nombre
+using System.ComponentModel;
+
+namespace Hilo
+{
+    public class Descargador
+    {
+        private string html;
+        private Uri direccion;
+
+        public Descargador(Uri direccion)
+        {
+            this.direccion = direccion;
+            this.html = "";
+        }
+
+        public void IniciarDescarga()
+        {
+            try
+            {
+                WebClient cliente = new WebClient();
+                cliente.DownloadProgressChanged += WebClientDownloadProgressChanged;
+                cliente.DownloadStringCompleted += WebClientDownloadCompleted;
+
+                cliente.DownloadStringAsync(this.direccion);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+
+        public delegate void DescargaEnProceso(int progreso);
+        public event DescargaEnProceso DescProgreso;
+
+        public delegate void DescargaFinalizada(string html);
+        public event DescargaFinalizada DescFinalizada;
+
+        
+        private void WebClientDownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            DescProgreso(e.ProgressPercentage);
+        }
+        private void WebClientDownloadCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            this.html = e.Result;
+
+            DescFinalizada(this.html);
+        }
+    }
+}
